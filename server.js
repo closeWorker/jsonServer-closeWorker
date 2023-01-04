5799dc35ad7879b4cb434c25dcf809049e3dc5ce
Antonio <netoifpe@gmail.com>
2023-01-04 11:44:56 -0300

    feat: creates the json server, with the routes users, services, comments, tokenAuth

---
+++ server.js
@@ -0,0 +1,27 @@
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+const cors = require("cors");
+const port = process.env.PORT || 3001;
+
+const app = jsonServer.create();
+const router = jsonServer.router("db.json");
+
+app.db = router.db;
+
+const rules = auth.rewriter({
+  users: 600,
+  services: 644,
+  comments: 644,
+  tokenAuth: 440,
+});
+
+app.use(cors());
+app.use(rules);
+app.use(auth);
+app.use(router);
+app.listen(port);
+
+console.log("Server is running on port:", port);
+
+/* A senha do usercoments@gmail.com é 123456 */
+/* A senha dos users testex é 123456ab */
